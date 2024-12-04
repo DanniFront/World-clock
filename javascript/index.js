1f2fe84a9e252d160a54c830b98e2804e7cc1eaf
DanniFront <mure.daniela@gmail.com>
2024-12-04 13:14:24 +0100

    Inject JS for dynamic date and time updates. Also added moment.js and timezone.

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,25 @@
+let newYorkElement = document.querySelector("#new-york");
+let newYorkTimeElement = newYorkElement.querySelector(".time");
+let newYorkDateElement = newYorkElement.querySelector(".date");
 
+let stockholmElement = document.querySelector("#stockholm");
+let stockholmTimeElement = stockholmElement.querySelector(".time");
+let stockholmDateElement = stockholmElement.querySelector(".date");
+
+function updateTime() {
+  let newYorkTime = moment().tz("America/New_York");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+
+  let stockholmTime = moment().tz("Europe/Stockholm");
+  stockholmTimeElement.innerHTML = stockholmTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+  stockholmDateElement.innerHTML = stockholmTime.format("MMMM Do YYYY");
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
